9c225790fc76a174c92e60fc0d936e6f68d0d910
vinicodex <mvnxr28@gmail.com>
2023-01-23 14:45:37 -0300

    apps modelviewset

--- apps/api/v1/viewsets.py
+++ apps/api/v1/viewsets.py
@@ -11,23 +11,8 @@ from rest_framework.authtoken.models import Token
 from .utils import check_subscription_plan
 
 
-class ApplicationViewSet(viewsets.GenericViewSet):
+class ApplicationViewSet(viewsets.ModelViewSet):
     serializer_class = ApplicationSerializer
     authentication_classes = (authentication.TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     queryset = Applications.objects.all()
-
-    def get_serializer(self, *args, **kwargs):
-        return self.serializer_class(*args, **kwargs)
-
-    def create(self, request, *args, **kwargs):
-        request_token = Token.objects.filter(key=request.auth).first()
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        subscription = Subscription.objects.get(user=request_token.user)
-        app_data, status_code = check_subscription_plan(subscription, request)
-        return Response(data=app_data, status=status_code)
-
-    def list(self, request, *args, **kwargs):
-        self.serializer_class(self.queryset, many=True)
-        return Response(self.serializer_class.data)
