91a0a4dc828823996af88232f2683ee3b164233d
vinicodex <mvnxr28@gmail.com>
2023-01-31 13:57:47 -0300

    add model to signals

--- users/signals.py
+++ users/signals.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from plans.models import Plan
 from test_crowdbotics_38402 import settings
 from subscriptions.models import Subscription
 
@@ -8,4 +9,12 @@ from subscriptions.models import Subscription
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_user_subscription(sender, instance, created, **kwargs):
     if created:
-        Subscription.objects.create(user=instance)
+        plan = Plan.objects.filter(name="FREE").first()
+        if not plan:
+            Subscription.objects.create(
+                                user=instance,
+                                plan=Plan.objects.create(
+                                    name="FREE",
+                                    description="FREE PLAN",
+                                    price=0))
+
